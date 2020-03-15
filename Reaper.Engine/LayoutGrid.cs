481a1d036ab4c0cb139a2a0e2320410e097861fb
Stephen Traskal <Straskal@gmail.com>
2020-03-15 16:23:45 -0400

    Update LayoutGrid.cs

--- Reaper.Engine/LayoutGrid.cs
+++ Reaper.Engine/LayoutGrid.cs
@@ -35,28 +35,31 @@ namespace Reaper.Engine
 
         internal void Add(WorldObject worldObject)
         {
-            GetCellPosition(worldObject.Position, out int cellX, out int cellY);
-
-            _cells[cellX, cellY].WorldObjects.Add(worldObject);
+            if (TryGetCellPosition(worldObject.Position, out int cellX, out int cellY))
+            {
+                _cells[cellX, cellY].WorldObjects.Add(worldObject);
+            }            
         }
 
         internal void Remove(WorldObject worldObject)
         {
-            GetCellPosition(worldObject.Position, out int cellX, out int cellY);
-
-            _cells[cellX, cellY].WorldObjects.Remove(worldObject);
+            if (TryGetCellPosition(worldObject.Position, out int cellX, out int cellY))
+            {
+                _cells[cellX, cellY].WorldObjects.Remove(worldObject);
+            }            
         }
 
         internal void Update(WorldObject worldObject)
         {
-            GetCellPosition(worldObject.PreviousPosition, out int previousCellX, out int previousCellY);
-            GetCellPosition(worldObject.Position, out int cellX, out int cellY);
+            if (TryGetCellPosition(worldObject.PreviousPosition, out int previousCellX, out int previousCellY)
+                && TryGetCellPosition(worldObject.Position, out int cellX, out int cellY)) 
+            {
+                if (previousCellX == cellX && previousCellY == cellY)
+                    return;
 
-            if (previousCellX == cellX && previousCellY == cellY)
-                return;
-
-            _cells[previousCellX, previousCellY].WorldObjects.Remove(worldObject);
-            _cells[cellX, cellY].WorldObjects.Add(worldObject);
+                _cells[previousCellX, previousCellY].WorldObjects.Remove(worldObject);
+                _cells[cellX, cellY].WorldObjects.Add(worldObject);
+            }
         }
 
         internal bool TestOverlapOffset(WorldObject worldObject, float xOffset, float yOffset)
@@ -69,13 +72,18 @@ namespace Reaper.Engine
                 worldObject.Bounds.Width,
                 worldObject.Bounds.Height);
 
-            GetCellPosition(testedPosition, out int cellX, out int cellY);
+            if (TryGetCellPosition(testedPosition, out int cellX, out int cellY)) 
+            {
+                return _cells[cellX, cellY].WorldObjects.Any(other => other != worldObject && other.IsSolid && other.Bounds.Intersects(bounds));
+            }
 
-            return _cells[cellX, cellY].WorldObjects.Any(other => other != worldObject && other.IsSolid && other.Bounds.Intersects(bounds));
+            return false;
         }
 
         internal bool TestOverlapOffset(WorldObject worldObject, float xOffset, float yOffset, out WorldObject overlappedWorldObject)
         {
+            overlappedWorldObject = null;
+
             var testedPosition = worldObject.Position + new Vector2(xOffset, yOffset);
 
             var bounds = new Rectangle(
@@ -84,23 +92,30 @@ namespace Reaper.Engine
                 worldObject.Bounds.Width,
                 worldObject.Bounds.Height);
 
-            GetCellPosition(testedPosition, out int cellX, out int cellY);
+            if (TryGetCellPosition(bounds.Center.ToVector2(), out int cellX, out int cellY))
+            {
+                overlappedWorldObject = _cells[cellX, cellY].WorldObjects.FirstOrDefault(other => other != worldObject && other.IsSolid && other.Bounds.Intersects(bounds));                
+            }
 
-            overlappedWorldObject = _cells[cellX, cellY].WorldObjects.FirstOrDefault(other => other != worldObject && other.IsSolid && other.Bounds.Intersects(bounds));
             return overlappedWorldObject != null;
         }
 
         internal WorldObject[] TestOverlap(Rectangle bounds)
         {
-            GetCellPosition(bounds.Center.ToVector2(), out int cellX, out int cellY);
+            if (TryGetCellPosition(bounds.Center.ToVector2(), out int cellX, out int cellY))
+            {
+                return _cells[cellX, cellY].WorldObjects.Where(other => other.Bounds.Intersects(bounds)).ToArray();
+            }
 
-            return _cells[cellX, cellY].WorldObjects.Where(other => other.Bounds.Intersects(bounds)).ToArray();
+            return Array.Empty<WorldObject>();
         }
 
-        private void GetCellPosition(Vector2 position, out int column, out int row)
+        private bool TryGetCellPosition(Vector2 position, out int column, out int row)
         {
             column = (int)(position.X / _cellSize);
             row = (int)(position.Y / _cellSize);
+
+            return column < _cells.GetLength(0) && row < _cells.GetLength(1);
         }
     }
 }
