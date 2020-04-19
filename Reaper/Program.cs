08769eb2f3901e20ae06f14d749eab6dd4ac3254
Stephen Traskal <Straskal@gmail.com>
2020-04-19 09:59:46 -0400

    further input cleanup

--- Reaper/Program.cs
+++ Reaper/Program.cs
@@ -17,10 +17,8 @@ namespace Reaper
 
             using (var game = new MainGame(settings))
             {
-                game.Singletons.Register(new InputManager(game));
+                game.Singletons.Register(new InputManager(game).SetUpBindings());
                 game.Singletons.Register(new GlobalInputHandler(game));
-                InputBindings.Initialize(game.Singletons.Get<InputManager>());
-                //game.LoadOgmoLayout("content/layouts/dungeontest.json");
                 game.LoadOgmoLayout(args[0]);
                 game.Run();
             }
---
+++ Reaper/Singletons/Input/InputBindings.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+namespace Reaper
+{
+    public static class InputBindings
+    {
+        public static InputManager SetUpBindings(this InputManager input)
+        {
+            // Global input. Some used for development.
+            input.NewPressedAction("toggleFullscreen").AddKey(Keys.F);
+            input.NewPressedAction("togglePaused").AddKey(Keys.P);
+            input.NewPressedAction("toggleDebug").AddKey(Keys.OemTilde);
+            input.NewPressedAction("exitGame").AddKey(Keys.Escape);
+
+            // Player input
+            input.NewAxisAction("horizontal").AddKeys(Keys.A, Keys.D);
+            input.NewAxisAction("vertical").AddKeys(Keys.W, Keys.S);
+            input.NewAxisAction("attackHorizontal").AddKeys(Keys.Left, Keys.Right);
+            input.NewAxisAction("attackVertical").AddKeys(Keys.Up, Keys.Down);
+
+            return input;
+        }
+    }
+}
