ac7883384fb6503207e703fcd80a43d86fab0047
Stephen Traskal <Straskal@gmail.com>
2020-04-19 08:58:01 -0400

    Update PlayerBehavior.cs

--- Reaper/Objects/Common/Behaviors/PlayerBehavior.cs
+++ Reaper/Objects/Common/Behaviors/PlayerBehavior.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Reaper.Engine;
+using System.Linq;
 
 namespace Reaper
 {
     public class PlayerBehavior : Behavior
     {
         private const float ATTACK_TIME_BUFFER = 0.5f;
+        private const float MAX_SPEED = 20f;
         private const float ACCELERATION = 30f;
         private const float DRAG = 0.8f;
 
@@ -26,6 +28,11 @@ namespace Reaper
 
         public int Health { get; private set; } = 5;
         public Vector2 Direction { get; private set; } = new Vector2(0, 1f);
+        public Vector2 Velocity 
+        {
+            get => _velocity;
+            set => _velocity = value;
+        }
 
         public override void Load()
         {
@@ -49,52 +56,84 @@ namespace Reaper
         public override void Tick(GameTime gameTime)
         {
             float elapsedTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
-            MoveAndAnimate(elapsedTime);
-            CheckAttack();
+
+            if (IsMoving(out var movementDirection)) 
+            {
+                UpdateVelocity(movementDirection, elapsedTime);
+                UpdateDirection(movementDirection);
+            }
+
+            SimulateDrag();
+            ApplyVelocity();
+            Animate(movementDirection);
+            
+            if (IsAttacking(out var attackDirection)) 
+            {
+                Attack(attackDirection);
+            }
         }
 
-        private void MoveAndAnimate(float elapsedTime)
+        private bool IsMoving(out Vector2 movementInput) 
         {
-            Vector2 movementInput = new Vector2(_horizontalAction.GetAxis(), _verticalAction.GetAxis());
-            bool isMoving = movementInput != Vector2.Zero;
+            movementInput = new Vector2(_horizontalAction.GetAxis(), _verticalAction.GetAxis());
 
             if (movementInput.Length() > 1f)
                 movementInput.Normalize();
 
-            if (isMoving)
-                Direction = movementInput;
+            return movementInput != Vector2.Zero;
+        }
 
+        private void UpdateVelocity(Vector2 movementInput, float elapsedTime) 
+        {
             _velocity += movementInput * ACCELERATION * elapsedTime;
-            _velocity *= DRAG;
+        }
 
+        private void SimulateDrag() 
+        {
+            _velocity *= DRAG;
+            _velocity.X = MathHelper.Clamp(_velocity.X, -MAX_SPEED, MAX_SPEED);
+            _velocity.Y = MathHelper.Clamp(_velocity.Y, -MAX_SPEED, MAX_SPEED);
+        }
+
+        private void ApplyVelocity()
+        {
             if (Owner.MoveAndCollideX(_velocity.X))
                 _velocity.X = 0f;
 
             if (Owner.MoveAndCollideY(_velocity.Y))
                 _velocity.Y = 0f;
-
-            _spriteSheetBehavior.Play(isMoving ? "walk" : "idle");
         }
 
-        private void CheckAttack() 
+        private void UpdateDirection(Vector2 movementInput) 
         {
-            if (IsAttacking(out Vector2 direction))
-            {
-                if (Game.TotalTime > _attackTimer)
-                {
-                    var proj = Layout.Objects.CreateProjectile(_projectileSpawnPoint.Value, direction, 200f, ignoreTags: "player");
-                    proj.ZOrder = Owner.ZOrder + 1;
-
-                    _fireSound.Play();
-                    _attackTimer = Game.TotalTime + ATTACK_TIME_BUFFER;
-                }
-            }
+            Direction = movementInput;
         }
 
-        private bool IsAttacking(out Vector2 direction) 
+        private void Animate(Vector2 movementInput)
+        {
+            _spriteSheetBehavior.Play(movementInput != Vector2.Zero ? "walk" : "idle");
+        }
+
+        private bool IsAttacking(out Vector2 direction)
         {
             direction = new Vector2(_attackHorizontalAction.GetAxis(), _attackVerticalAction.GetAxis());
+
+            if (direction.Length() > 1f)
+                direction.Normalize();
+
             return direction != Vector2.Zero;
+        }
+
+        private void Attack(Vector2 direction) 
+        {
+            if (Game.TotalTime > _attackTimer)
+            {
+                var proj = Layout.Objects.CreateProjectile(_projectileSpawnPoint.Value, direction, 200f, ignoreTags: "player");
+                proj.ZOrder = Owner.ZOrder + 1;
+
+                _fireSound.Play();
+                _attackTimer = Game.TotalTime + ATTACK_TIME_BUFFER;
+            }
         }
 
         private void OnDamaged(DamageableBehavior.DamageInfo info)
